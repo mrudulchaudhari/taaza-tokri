5f9bf165e79a0887cff161f0e9f0d2a19420a85b
Mrudul <mrudulchaudhari0608@gmail.com>
2025-07-25 22:07:15 +0530

    added views and urls for vendor
    basic version

--- vendorsite/vendor/views.py
+++ vendorsite/vendor/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def vendor_dashboard(request):
+    return render(request, 'vendor/dashboard.html')
