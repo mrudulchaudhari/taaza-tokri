8737448f8d1cc0d1d20adf2b37aacc78cde53a06
Mrudul <mrudulchaudhari0608@gmail.com>
2025-07-27 23:11:10 +0530

    for deployment

--- vendorsite/vendorsite/settings.py
+++ vendorsite/vendorsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7^p+_nh17)5fh*5u700#1@aupc9qs%62s&j8ni53((*c$+nucm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'vendorsite.urls'
